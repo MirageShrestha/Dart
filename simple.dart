4cb15966579a0363c35ccb966492d9ae6017c0cd
MirageShrestha <128497517+MirageShrestha@users.noreply.github.com>
2024-08-29 01:21:51 +0545

    var final const

--- simple.dart
+++ simple.dart
@@ -6,3 +6,26 @@ void main() {
   greeting='${greeting} hello';
   print(greeting);
   }
+
+void main(){
+  
+  var value = '10';
+  final value1 = '11';
+  const value2 ='17';
+  
+  print(value);
+  print(value1);
+  print(value2);
+  
+  
+  value = '15';
+  value1='200';
+  value2 = '199';
+  
+  print(value);
+  print(value1);
+  print(value2);
+  
+  
+}
+
